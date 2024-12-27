2a9e19421e958ede5bd6b9e35ddaeece2ba5b952
1dans <idansuwu@gmail.com>
2024-12-27 13:52:09 +0200

    all tasks done

--- pointer_hw1.cpp
+++ pointer_hw1.cpp
@@ -7,19 +7,25 @@ void fillArray(int* ptr, int size) {
         *(ptr+i) = 1 + rand() % 10;
     }
 }
-/* #1
-void copyArray(int *ptr, int*p, int size) {
+/* void copyArray(int *ptr, int*p, int size) { // #1
     for (int i = 0; i < size; ++i) {
         *(p + i) = *(ptr + i);
     }
 }*/
 
-/* void reverseArray(int* ptr, int size) { #2
+/* void reverseArray(int* ptr, int size) { // #2
     for (int i = size-1; i >= 0; --i) {
         cout << *(ptr + i) << ' ';
     }
 }*/
 
+void reverseCopyArray(int* ptr, int*p, int size) { // #3 
+    int reverse_i = size-1;
+    for (int i = 0; i < size; ++i) {
+        *(p+i) = *(ptr + reverse_i);
+        --reverse_i;
+    }
+}
 void displayArray(int* ptr, int size) {
     for (int i = 0; i < size; ++i) {
         cout << *(ptr+i) << ' ';
@@ -38,16 +44,17 @@ int main()
     cout << "Enter the size of the array: ";
     cin >> size;
     int* ptr = new int[size];
-    // int* p = new int[size]; #1
+    int* p = new int[size]; // #1 & #3
     fillArray(ptr, size);
     displayArray(ptr, size);
     cout << endl;
+    reverseCopyArray(ptr, p, size);
     // reverseArray(ptr, size); #2
     //copyArray(ptr, p, size); #1
     
-    // displayArray(p, size); #1
+    displayArray(p, size); // #1 & #3
     del(ptr);
-    //del(p); #1
+    del(p); // #1 & #3
 
     //#2
 }
